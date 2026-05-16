8e324c8ad3755cdc04a61a96301de42c40a19697
Carlos Fernandez <fdezserranocarlos@gmail.com>
2026-05-17 01:08:21 +0200

    ex 1.7

--- logs-app/main.py
+++ logs-app/main.py
@@ -1,25 +1,24 @@
-import time
-import logging
+import os
 import string
 import secrets
+import uvicorn
+from datetime import datetime
+from fastapi import FastAPI
 
-def make_random_string(length: int = 16) -> str:
+app = FastAPI()
+
+def make_random_string(length: int = 24) -> str:
     alphabet = string.ascii_letters + string.digits
     return "".join(secrets.choice(alphabet) for _ in range(length))
 
-def setup_logging() -> logging.Logger:
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s: %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-    return logging.getLogger("random_string_app")
+@app.get("/")
+def read_root():
+    return {
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "token": make_random_string()
+    }
 
-
-if __name__=="__main__":
-    logger = setup_logging()
-    token = make_random_string(24)
-    while True:
-        logger.info(token)
-        time.sleep(5)
-
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    print(f"Server started in port {port}")
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
